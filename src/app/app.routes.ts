59ef940d252016c97bfcfe5fd5ec6959cc5b81ff
maicol <pirlo1121s@gmail.com>
2025-01-21 13:32:48 -0500

    Create card

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent, data: {title: 'home'}},
+    { path: '**', redirectTo: '' }
+];
