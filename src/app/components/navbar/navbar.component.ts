bd29e3815e949ab5142d634c9f99fd436e46e4ed
pirlo <pirlo@linux-dev.168.1.5>
2025-01-23 12:26:25 -0500

    navbar content

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  title: string = '</Portfolio>'
 
-}
+  ngAfterViewInit(): void {
+    this.toggleMenu();
+  }
+
+  private toggleMenu(): void {
+    const navbar: HTMLElement | null = document.querySelector('.navbar');
+    const burger: HTMLElement | null = document.querySelector('.burger');
+
+    if (burger && navbar) {
+      const span: HTMLElement | null = burger.querySelector('span');
+      if (span) {
+        span.addEventListener('click', (e: Event) => {
+          navbar.classList.toggle('show-nav');
+        });
+      }
+
+      // Evitamos que el evento del logo afecte al menú
+      const logo: HTMLElement | null = document.querySelector('.navbar .logo');
+      if (logo) {
+        logo.addEventListener('click', (e: Event) => {
+          e.stopPropagation(); // Evita que el evento se propague al contenedor principal (nav)
+        });
+      }
+
+      // Bonus
+      const navbarLinks: NodeListOf<HTMLAnchorElement> = document.querySelectorAll('.links_menu');
+      navbarLinks.forEach(link => {
+        link.addEventListener('click', (e: Event) => {
+          navbar.classList.toggle('show-nav');
+        });
+      });
+    }
+  }}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
