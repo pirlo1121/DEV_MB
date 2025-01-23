a3e7e3d11be7e3fc9da74a29355a5f13f8ee2153
pirlo <pirlo@linux-dev.168.1.5>
2025-01-23 14:15:48 -0500

    first home banner

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
 
+  jsonObject = {
+    name: 'juanito',
+    age: 25,
+    country: 'Mexico'
+  };
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { BannerHomeComponent } from '../../components/banner-home/banner-home.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardComponent, NavbarComponent],
+  imports: [CardComponent, NavbarComponent, BannerHomeComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
