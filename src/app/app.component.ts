e59f9809887c2d09418c3699cdbca9ef7db87424
Maicol <pirlo1121s@gmail.com>
2025-01-28 09:53:28 -0500

    change color menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
 import { CommonModule } from '@angular/common';
 import { BackgroundComponent } from './components/background/background.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, BackgroundComponent],
+  imports: [RouterOutlet, CommonModule, BackgroundComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
