d6e52bee350518f49b5c8a6554eab0b58f2fc3a5
pirlo <pirlo@linux-dev.168.1.5>
2025-01-27 22:34:36 -0500

    background one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
 import { CommonModule } from '@angular/common';
+import { BackgroundComponent } from './components/background/background.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent, CommonModule],
+  imports: [RouterOutlet, CommonModule, BackgroundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/background/background.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-background',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './background.component.html',
+  styleUrl: './background.component.css'
+})
+export class BackgroundComponent {
+  stars: Array<{ x: number, y: number, size: number, duration: number }> = [];
+
+  ngOnInit(): void {
+    this.createStars();
+  }
+
+  createStars() {
+    const numberOfStars = 500;
+    for (let i = 0; i < numberOfStars; i++) {
+      this.stars.push({
+        x: Math.random() * window.innerWidth,
+        y: Math.random() * window.innerHeight,
+        size: Math.random() * 3 + 1, // Tamaño aleatorio
+        duration: Math.random() * 2 + 1 // Duración aleatoria para el parpadeo
+      });
+    }
+  }
+}
