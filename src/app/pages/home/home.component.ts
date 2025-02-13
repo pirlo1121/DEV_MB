2171ad71a79d2e46872da58697310c2863967aec
Maicol <pirlo1121s@gmail.com>
2025-02-13 18:17:22 -0500

    change colors

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { BannerHomeComponent } from '../../components/banner-home/banner-home.component';
-import { Card2Component } from '../../components/card2/card2.component';
 import { AboutMeComponent } from '../../components/about-me/about-me.component';
 import { ProjectsComponent } from '../../components/projects/projects.component';
 import { ContactComponent } from '../../components/contact/contact.component';
@@ -9,7 +8,7 @@ import { ContactComponent } from '../../components/contact/contact.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ NavbarComponent, BannerHomeComponent, Card2Component, AboutMeComponent, ProjectsComponent, ContactComponent],
+  imports: [ NavbarComponent, BannerHomeComponent, AboutMeComponent, ProjectsComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
