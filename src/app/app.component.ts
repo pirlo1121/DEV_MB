a9a1bd2b0db3d5045aee2498b29c630ba561200c
Maicol <pirlo1121s@gmail.com>
2025-02-13 18:52:56 -0500

    change fonts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BackgroundComponent } from './components/background/background.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BackgroundComponent, FooterComponent],
+  imports: [RouterOutlet, BackgroundComponent, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
